7bff6a6f8dd775fb556f3fc2eba98c60bcdf4d57
Dmitry Grinshteyn <dgrin2005@mail.ru>
2019-03-05 11:00:55 +0600

    L07.1

---
+++ L07.1/src/main/java/ru/otus/ATM.java
@@ -0,0 +1,104 @@
+package ru.otus;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ATM {
+
+    private Map<Integer, Integer> banknoteCells;
+
+    public ATM() {
+        banknoteCells = new TreeMap<>(Collections.reverseOrder());
+        banknoteCells.put(10, 0);
+        banknoteCells.put(50, 0);
+        banknoteCells.put(100, 0);
+        banknoteCells.put(200, 0);
+        banknoteCells.put(500, 0);
+        banknoteCells.put(1000, 0);
+        banknoteCells.put(2000, 0);
+        banknoteCells.put(5000, 0);
+    }
+
+    public Map<Integer, Integer> getBanknoteCell() {
+        return banknoteCells;
+    }
+
+    private void setBanknoteCell(Map<Integer, Integer> banknoteCells) {
+        this.banknoteCells = banknoteCells;
+    }
+
+    void putBanknote(Integer banknote) {
+        if (banknoteCells.containsKey(banknote)) {
+            banknoteCells.replace(banknote, banknoteCells.get(banknote) + 1);
+        } else {
+            System.out.println("Ошибка. Нет такого номинала банкнот");
+        }
+
+    }
+
+    private void getBanknote(Integer banknote) {
+        if (banknoteCells.containsKey(banknote)) {
+            if (getBanknoteAmount(banknote) > 0) {
+                banknoteCells.replace(banknote, banknoteCells.get(banknote) - 1);
+            } else {
+                System.out.println("Недостаточно средств в банкомате");
+            }
+        } else {
+            System.out.println("Ошибка. Нет такого номинала банкнот");
+        }
+    }
+
+    public Long getBalance() {
+        Long balance= 0L;
+        for (Map.Entry<Integer, Integer> cell : banknoteCells.entrySet()) {
+            balance += cell.getKey() * cell.getValue();
+        }
+        return balance;
+    }
+
+    public Integer getBanknoteAmount(Integer banknote) {
+        Integer amount = 0;
+        if (banknoteCells.containsKey(banknote)) {
+            amount = banknoteCells.get(banknote);
+        } else {
+            System.out.println("Ошибка. Нет такого номинала банкнот");
+        }
+        return amount;
+    }
+
+    public ATM getSum(Integer sum) {
+        ATM banknotes = new ATM();
+        if (sum <= 0) {
+            System.out.println("Затребованная сумма должна быть больше нуля");
+        } else {
+            if (sum <= getBalance()) {
+                for (Integer key : banknoteCells.keySet()) {
+                    while (getBanknoteAmount(key) > 0 && sum >= key) {
+                        if (sum >= key) {
+                            banknotes.putBanknote(key);
+                            getBanknote(key);
+                            sum -= key;
+                        }
+                    }
+                }
+                if (sum > 0) {
+                    banknotes = new ATM();
+                    System.out.println("Невозможно выдать затребованную сумму");
+                }
+            } else {
+                System.out.println("Недостаточно средств в банкомате");
+            }
+        }
+        return  banknotes;
+    }
+
+    @Override
+    public String toString() {
+        return "ATM {" +
+                "Банкноты = " + banknoteCells +
+                ", Баланс=" + getBalance() +
+                '}';
+    }
+}
+
---
+++ L07.1/src/main/java/ru/otus/MainClass.java
@@ -0,0 +1,39 @@
+package ru.otus;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ATM atm = new ATM();
+        fillAtm(atm);
+        System.out.println("Состояние " + atm);
+        System.out.println();
+
+        moneyRequest(atm, -10);
+        moneyRequest(atm, 123000);
+        moneyRequest(atm, 7800);
+        moneyRequest(atm, 123);
+        moneyRequest(atm, 50000);
+
+    }
+
+    static void fillAtm(ATM atm) {
+        for(int i = 0; i < 20; i++) {
+            atm.putBanknote(10);
+            atm.putBanknote(50);
+            atm.putBanknote(100);
+            atm.putBanknote(200);
+            atm.putBanknote(500);
+            atm.putBanknote(1000);
+            atm.putBanknote(2000);
+            atm.putBanknote(5000);
+        }
+    }
+
+    static void moneyRequest(ATM atm, int sum) {
+        System.out.println("Затребовано " + sum);
+        System.out.println("Выдано " + atm.getSum(sum));
+        System.out.println("Состояние " + atm);
+        System.out.println();
+    }
+}
