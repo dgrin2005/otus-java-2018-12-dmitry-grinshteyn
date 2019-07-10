47c1a653e08be6778ef1a8f025f6153dd7d7a51a
dgrin2005 <dgrin2005@mail.ru>
2019-07-10 13:39:12 +0200

    Added properties names

--- ProjectWork/src/main/java/ru/otus/utilities/DBUtilites.java
+++ ProjectWork/src/main/java/ru/otus/utilities/DBUtilites.java
@@ -13,12 +13,18 @@ import java.util.Properties;
 
 public class DBUtilites {
 
-    private final static String propertiesFileName = "mongodb.properties";
+    private final static String PROPERTIES_FILENAME = "mongodb.properties";
+    private final static String PROPERTIE_NAME_HOST = "host";
+    private final static String PROPERTIE_NAME_PORT = "port";
+    private final static String PROPERTIE_NAME_USERNAME = "username";
+    private final static String PROPERTIE_NAME_PASSWORD = "password";
+    private final static String PROPERTIE_NAME_DATABASE = "dbname";
+    private final static String PROPERTIE_NAME_DDL = "ddl";
     private static Properties properties;
 
     static {
         properties = new Properties();
-        File file = new File(propertiesFileName);
+        File file = new File(PROPERTIES_FILENAME);
         try {
             properties.load(new FileInputStream(file));
         } catch (IOException e) {
@@ -28,13 +34,13 @@ public class DBUtilites {
 
     public static MongoClient getMongoDbConnection() throws MyOrmException {
 
-        final String host = properties.getProperty("host");
-        final String port = properties.getProperty("port");
-        final String username = properties.getProperty("username");
-        final String password = properties.getProperty("password");
+        final String host = properties.getProperty(PROPERTIE_NAME_HOST);
+        final String port = properties.getProperty(PROPERTIE_NAME_PORT);
+        final String username = properties.getProperty(PROPERTIE_NAME_USERNAME);
+        final String password = properties.getProperty(PROPERTIE_NAME_PASSWORD);
 
         if (host == null) {
-            throw new MyOrmException("Host not found in file " + propertiesFileName);
+            throw new MyOrmException("Host not found in file " + PROPERTIES_FILENAME);
         }
         StringBuilder connString = new StringBuilder("mongodb://");
         if (username != null && password != null) {
@@ -48,9 +54,9 @@ public class DBUtilites {
     }
 
     public static String getDBName() throws MyOrmException {
-        final String dbname = properties.getProperty("dbname");
+        final String dbname = properties.getProperty(PROPERTIE_NAME_DATABASE);
         if (dbname == null) {
-            throw new MyOrmException("Database name not found in file " + propertiesFileName);
+            throw new MyOrmException("Database name not found in file " + PROPERTIES_FILENAME);
         }
         return dbname;
     }
@@ -64,7 +70,7 @@ public class DBUtilites {
     }
 
     public static Ddl getDdl() {
-        final String ddlName = properties.getProperty("ddl");
+        final String ddlName = properties.getProperty(PROPERTIE_NAME_DDL);
         if (ddlName == null) {
             return Ddl.CREATE;
         }
